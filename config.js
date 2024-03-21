f48b7c6dc6aab6c8a43ba667727dbda3a2081208
manjutimes <manjutimes@gmail.com>
2024-03-21 17:14:29 +0530

    21/3/2024 17:14

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.config = {
     signInPageUrl: 'https://43c211e5-18f7-4fa9-89ae-db4f1e9656a3.e1-us-east-azure.choreoapps.dev',
-    apiUrl: 'https://6d50e95b-721e-4f61-9e29-f037436d2f05-dev.e1-us-east-azure.choreoapis.dev/zdur/ballerinaservice/endpoint-9090-803/v1.0'
+    apiUrl: '/choreo-apis/zdur/ballerinaservice/endpoint-9090-803/v1.0'
       
     };
